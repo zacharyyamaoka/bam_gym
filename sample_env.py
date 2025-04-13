52b3e8be6f7b45ed78c01d750de9d2237f9d7b28
Zach Yamaoka <zach.yamaoka@gmail.com>
2025-04-13 07:24:00 -0700

    making slow but good progress I think

--- sample_env.py
+++ sample_env.py
@@ -1,12 +1,13 @@
 import gymnasium as gym
 import bam_gym_env
-# env = gym.make("CartPole-v1", render_mode="human")
-env = gym.make("bam/GridWorld-v0", render_mode="human")
+env = gym.make("CartPole-v1", render_mode="human")
+# env = gym.make("bam/GridWorld-v0")
 
 observation, info = env.reset(seed=42)
 for _ in range(100):
     action = env.action_space.sample()
     observation, reward, terminated, truncated, info = env.step(action)
+    print(type(observation))
 
     if terminated or truncated:
         observation, info = env.reset()
